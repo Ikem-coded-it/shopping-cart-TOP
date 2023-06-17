5aed77bf4835c7173ca1b169a780c9b74d1b879d
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-06-17 16:13:21 +0100

    finished homepage mobile view

--- src/components/LandingPage/Main/index.js
+++ src/components/LandingPage/Main/index.js
@@ -1,14 +1,24 @@
+import {useState, useEffect, useRef} from "react"
 import "./styles.css"
 
 export default function Main() {
+  const [background, setBackground] = useState("main-background")
+
+  useEffect(() => {
+    const screenWidth = window.innerWidth
+    if (screenWidth < 600) {
+      setBackground("main-background-mobile")
+    }
+  }, [])
+
   return(
-    <div className="main-background">
+    <div className={background}>
       <div className="write-up-container">
         <div className="write-up">
           <div className="welcome">Welcome</div>
           <div className="to">to</div>
-          <div className="baller-store">Baller Store</div>
-          <p className="para">Ready to take your game to the next level?</p>
+          <div className="baller-store">BallerStore</div>
+          <div className="para">Ready to take your game to the next level?</div>
         </div>
         <button>Get Some Balls</button>
       </div>
--- src/components/LandingPage/NavBar/index.js
+++ src/components/LandingPage/NavBar/index.js
@@ -1,3 +1,5 @@
+import {useState, useEffect} from "react";
+
 import "./styles.css"
 
 function NavBar() {
