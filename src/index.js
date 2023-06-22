c041bdbd2a3ed80b864ac1d8af0d7010fc68bace
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-06-22 23:46:27 +0100

    lil fix

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
